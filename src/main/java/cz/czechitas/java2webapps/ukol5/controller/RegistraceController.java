6ce57ceb8f84b5e08b5938dd47747559d0b60b31
zvolankm <zvolankovamartina@gmail.com>
2022-11-14 16:04:27 +0100

    Detsky tabor registrace

--- src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
@@ -1,7 +1,17 @@
 package cz.czechitas.java2webapps.ukol5.controller;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.time.Period;
 
 /**
  * Kontroler obsluhující registraci účastníků dětského tábora.
@@ -10,8 +20,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class RegistraceController {
 
   @GetMapping("/")
-  public String formular() {
-    return "formular";
+  public ModelAndView formular() {
+    ModelAndView modelAndView = new ModelAndView("/formular");
+    modelAndView.addObject("form", new RegistraceForm());
+    return modelAndView;
   }
+  @PostMapping("")
+  public Object form(@Valid @ModelAttribute("form") RegistraceForm form, BindingResult bindingResult) {
 
+    Period period = form.getDatumNarozeni().until(LocalDate.now());
+    int vek = period.getYears();
+
+    if (vek < 9) {
+      return "/nevyhovujici-vek";
+    }
+
+    if (vek > 15){
+      return "/nevyhovujici-vek";
+    }
+
+
+    if (bindingResult.hasErrors()) {
+      return "/formular";
+    }
+    return new ModelAndView("/rekapitulace");
+  }
+  @PostMapping("/local-date")
+  public void localDate(@RequestParam("localDate")
+                          @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate localDate) {
+  }
 }
---
+++ src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceForm.java
@@ -0,0 +1,80 @@
+package cz.czechitas.java2webapps.ukol5.controller;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+
+public class RegistraceForm {
+    @NotBlank
+    @Size(min = 3, max = 150)
+    private String jmeno;
+    @NotBlank
+    @Size(min = 3, max = 150)
+    private String prijmeni;
+    @DateTimeFormat(pattern = "yyyy/mm/dd")
+    private LocalDate datumNarozeni;
+    @NotBlank
+    private String pohlavi;
+    @NotBlank
+    private String turnus;
+    @Email
+    private String emailNaRodice;
+    private String telefonNaRodice;
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public String getPrijmeni() {
+        return prijmeni;
+    }
+
+    public void setPrijmeni(String prijmeni) {
+        this.prijmeni = prijmeni;
+    }
+
+    public LocalDate getDatumNarozeni() {
+        return datumNarozeni;
+    }
+
+    public void setDatumNarozeni(LocalDate datumNarozeni) {
+        this.datumNarozeni = datumNarozeni;
+    }
+
+    public String getPohlavi() {
+        return pohlavi;
+    }
+
+    public void setPohlavi(String pohlavi) {
+        this.pohlavi = pohlavi;
+    }
+
+    public String getTurnus() {
+        return turnus;
+    }
+
+    public void setTurnus(String turnus) {
+        this.turnus = turnus;
+    }
+
+    public String getEmailNaRodice() {
+        return emailNaRodice;
+    }
+
+    public void setEmailNaRodice(String emailNaRodice) {
+        this.emailNaRodice = emailNaRodice;
+    }
+
+    public String getTelefonNaRodice() {
+        return telefonNaRodice;
+    }
+
+    public void setTelefonNaRodice(String telefonNaRodice) {
+        this.telefonNaRodice = telefonNaRodice;
+    }
+}
